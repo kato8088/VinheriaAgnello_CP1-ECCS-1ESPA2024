cf39b53b0f162bc9d820d91c940744d99f5ba53f
Kato <112272544+kato8088@users.noreply.github.com>
2024-09-23 14:03:04 -0300

    limpeza de codigo e implementacao do map
    função map adicionada no código & limpeza
    v2

--- VinheriaArduino.ino
+++ VinheriaArduino.ino
@@ -1,14 +1,9 @@
 /*
-(tinkercad) LDR - valor analogico na prática, de 54 a 974 (com 5v) (tinkercad)
-(tinkercad) leitura resistencia LDR (tinkercad)
-0% - 506ohms
-50% - 1k
-100% - 180k
-
-referencia ldr teste: ± 20% do limite
-
-analogRead mede voltagem
-referencia: 0.0049 volts (4.9 mV) per unit
+2do:
+- ajustar os valores do ldr na prática, no simulador está instável
+- ajustar o delay dos buzzers
+- fazer sons diferentes para atencao e alerta
+- colocar lcd
 */
 
 #define LDR_PIN A5
@@ -18,7 +13,8 @@ referencia: 0.0049 volts (4.9 mV) per unit
 #define debugLed 13
 #define buzzer 5
 
-int ldrValue;
+int intensidadeLDR;
+
 
 void setup(){
   Serial.begin(9600);
@@ -31,52 +27,43 @@ void setup(){
 }
 
 int luxTick(){
-  if (analogRead(LDR_PIN) > 849) {
-    Serial.println("Luminosidade: Alerta!");
+  intensidadeLDR = map(analogRead(LDR_PIN), 40, 999, 0, 100);
+
+  if (intensidadeLDR > 50) {
+    Serial.println(" Luminosidade: Alerta!");
     digitalWrite(ledVermelho, HIGH);
     digitalWrite(ledAmarelo, LOW);
     digitalWrite(ledVerde, LOW);
-    return 1;
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+    //fine tune in real hardware l8r
+    digitalWrite(buzzer, LOW);
   }
-  if ( 849 > analogRead(LDR_PIN) and analogRead(LDR_PIN) > 799) {
-    Serial.println("Luminosidade: Atencao!");
+  if (41 < intensidadeLDR and intensidadeLDR < 50) {
+    Serial.println(" Luminosidade: Atencao!");
     digitalWrite(ledVermelho, LOW);
     digitalWrite(ledAmarelo, HIGH);
     digitalWrite(ledVerde, LOW);
-    return 0;
+    digitalWrite(buzzer, HIGH);
+    delay(500);
+    digitalWrite(buzzer, LOW);
+    //fine tune in real hardware l8r
   }
-  if (analogRead(LDR_PIN) < 800) {
-    Serial.println("Luminosidade: normal");
+  if (intensidadeLDR < 40) {
+    Serial.println(" Luminosidade: normal");
     digitalWrite(ledVermelho, LOW);
     digitalWrite(ledAmarelo, LOW);
     digitalWrite(ledVerde, HIGH);
-    return 0;
   }
 }
 
 void loop(){
   
   luxTick();
-  //debug start
-  Serial.println(luxTick());
-  //debug end
-  while(luxTick() == 1){
-    digitalWrite(buzzer, HIGH);
-    delay(3000);
-    digitalWrite(buzzer, LOW);
-    // debug start
-    digitalWrite(debugLed, HIGH);
-    // debug end
-  }
-  else {
 
-    //debug start
-  digitalWrite(debugLed, LOW);
-    //debug end
-  }
-  ldrValue = analogRead(LDR_PIN);
-  //debug
-  Serial.println(ldrValue);
-  delay(500);
-  //debug
+  Serial.print("Intensidade da Luz: ");
+  Serial.println(intensidadeLDR);
+  
+  delay(1000);
+  // fine tune in real hardware l8r
 }
